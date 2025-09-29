a581c7e9e259ed37cb7bbf0eeb700f42a7d07af9
J. "Dorian Greyson" L. <greysondn@gmail.com>
2025-09-29 06:39:15 -0400

    WIP - add missing type hints

--- worlds/spyro/bhhelpers.py
+++ worlds/spyro/bhhelpers.py
@@ -1,4 +1,9 @@
 from aphelpers import APLocation
+from typing import List
+from typing import Tuple
+
+BHMemoryAddress = Tuple[int, int, str]
+"""(address, length_in_bytes, region)"""
 
 class BHLocation(APLocation):
     def __init__(self, game:str, address:int, length:int):
@@ -10,17 +15,17 @@ class BHMemoryEntry():
         self.raw_data:bytes = b""
         self.address:int = address
         self.length:int = length
-        self.region = "MainRam"
+        self.region:str = "MainRam"
     
-    def __int__(self):
+    def __int__(self) -> int:
         return int.from_bytes(self.raw_data, "little")
 
 class BHMemoryEntryList(list):
     def __init__(self):
         super().__init__()
     
-    def to_batched_read_list(self) -> list[tuple[int, int, str]]:
-        ret:list[tuple[int, int, str]] = []
+    def to_batched_read_list(self) -> List[BHMemoryAddress]:
+        ret:list[BHMemoryAddress] = []
         
         for item in self:
             ret.append((item.address, item.length, item.region))

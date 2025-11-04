19ca32248bc4e2f06f4bdf7778469fc3f5ea1de2
J. "Dorian Greyson" L. <greysondn@gmail.com>
2025-11-03 20:10:27 -0500

    define __all__ for appetite.ap

---
+++ worlds/spyro/appetite/ap/__init__.py
@@ -0,0 +1,55 @@
+from appetite.ap.config import (
+    Config,
+)
+
+from appetite.ap.container import (
+    Container,
+)
+
+from appetite.ap.core import (
+    CollectionState as CoreCollectionState,
+    Location as CoreLocation,
+    LocationGroups as CoreLocationGroups,
+    LocationList as CoreLocationList,
+)
+
+from appetite.ap.gameworld import (
+    GameWorld,
+)
+
+from appetite.ap.item import (
+    Item,
+)
+
+from appetite.ap.location import (
+    Location,
+)
+
+from appetite.ap.manager import (
+    IndexManager,
+)
+
+from appetite.ap.player import (
+    Player,
+)
+
+from appetite.ap.region import (
+    Connection,
+    Region,
+)
+
+__all__ = [
+    "Config",
+    "Connection",
+    "Container",
+    "CoreCollectionState",
+    "CoreLocation",
+    "CoreLocationGroups",
+    "CoreLocationList",
+    "GameWorld",
+    "IndexManager",
+    "Item",
+    "Location",
+    "Player",
+    "Region",
+]
